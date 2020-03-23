62cd25594adb15a9b8b1e2d55c48912744062bf3
louis baptiste <louisvis@ymail.com>
2020-03-23 23:50:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-/function BoardMember(name, homeState, training){
-/  this.name = name
+ function BoardMember(name, homeState, training){
+   this.name = name
    this.homeState = homeState
-  this.training = training
+   this.training = training
 }
+
    BoardMember.prototypes.veto = function(){
    return 'No, I must disagree'
-//   }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+   BoardMember.prototypes.approve = function(){
+     return 'You can do that!'
+   }
+   BoardMember.prototypes.doCharity = function(){
+    return "I like to help people."
+   }
+
+   BoardMember.prototypes.releasePressStatement = function(){
+    return "You will see great things from Scuber."
+   }
+
+  BoardMember.prototypes.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
