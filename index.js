8ca51a6fb37b802e2b30ff45e853424cbe924894
louis baptiste <louisvis@ymail.com>
2020-03-23 23:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
+/function BoardMember(name, homeState, training){
+/  this.name = name
+   this.homeState = homeState
+  this.training = training
+}
+   BoardMember.prototypes.veto = function(){
+   return 'No, I must disagree'
 //   }
 //   this.approve = function(){
 //     return 'You can do that!'
